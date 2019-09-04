39960d8c27c9993c626a4efd9ed2eee673e7ad4f
VJitaru <51636737+VJitaru@users.noreply.github.com>
2019-09-04 20:38:38 +0100

    Update script.js

--- javascript/script.js
+++ javascript/script.js
@@ -4,7 +4,7 @@ window.onscroll = function() {
   if (prevScrollpos > currentScrollPos) {
     document.getElementById("navbar-top").style.top = "0";
   } else {
-    document.getElementById("navbar-top").style.top = "-50px";
+    document.getElementById("navbar-top").style.top = "-60px";
   }
   prevScrollpos = currentScrollPos;
 }
